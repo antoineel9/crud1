386a57d1da4ec3407fce1a5c342308ec7726f8d0
Antoine Williams <54339074+antoineel9@users.noreply.github.com>
2019-10-08 12:57:44 -0500

    Add files via upload

---
+++ Insurance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD1.Pages
+{
+    public class Insurance
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DID { get; set; }
+
+
+        public string DName { get; set; }
+    }
+}
