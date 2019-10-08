d91bce847296440607d0357a7d86114683d488e0
Antoine Williams <54339074+antoineel9@users.noreply.github.com>
2019-10-08 12:57:10 -0500

    Add files via upload

---
+++ HR.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD1.Pages
+{
+    public class HR
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DID { get; set; }
+
+        public string DName { get; set; }
+    }
+}
